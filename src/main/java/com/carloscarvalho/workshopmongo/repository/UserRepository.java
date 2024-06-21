90792879568210c336e64fa04032eb51e1988c70
Carlos Felipe de Carvalho <carlosfelipecarvalhosantiago@gmail.com>
2024-06-21 16:38:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/carloscarvalho/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.carloscarvalho.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carloscarvalho.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
