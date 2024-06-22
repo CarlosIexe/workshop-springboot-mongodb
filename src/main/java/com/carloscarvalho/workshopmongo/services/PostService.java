6f2edbdf1f89356c1402f4d35b3f2c7df32e4de7
Carlos Felipe de Carvalho <carlosfelipecarvalhosantiago@gmail.com>
2024-06-22 11:07:51 -0300

    Consulta com varios criterios

--- src/main/java/com/carloscarvalho/workshopmongo/services/PostService.java
+++ src/main/java/com/carloscarvalho/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.carloscarvalho.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,11 @@ public class PostService {
 
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date min, Date max) {
+		max = new Date(max.getTime() + 24 * 60 * 60 * 1000);
+		return repo.FullSearch(text, min, max);
 	}
 
 }
