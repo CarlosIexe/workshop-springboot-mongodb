a29d61a4983c3b8e4e1d84f84f2ca1899a2a0e7a
Carlos Felipe de Carvalho <carlosfelipecarvalhosantiago@gmail.com>
2024-06-22 09:40:01 -0300

    Obtendo post por id

---
+++ src/main/java/com/carloscarvalho/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.carloscarvalho.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carloscarvalho.workshopmongo.domain.Post;
+import com.carloscarvalho.workshopmongo.repository.PostRepository;
+import com.carloscarvalho.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+}
